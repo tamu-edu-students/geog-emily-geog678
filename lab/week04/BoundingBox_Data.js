2c5b3f1b5c189c38712f1e28fd774ed7ec82f3cd
emilymcdonald-tamu <emilymcdonald@tamu.edu>
2025-11-16 20:36:55 -0600

    Add files via upload

---
+++ lab/week04/BoundingBox_Data.js
@@ -0,0 +1,10 @@
+var theJSON =
+{
+	"data": [
+		{"pointedId": 1, "lat": 30.61586, "lon": -96.35557},
+		{"pointedId": 2, "lat": 30.61724, "lon": -96.3368},
+		{"pointedId": 3, "lat": 30.60649, "lon": -96.35613},
+		{"pointedId": 4, "lat": 30.60718, "lon": -96.33817},
+		{"pointedId": 5, "lat": 30.61479, "lon": -96.34729}
+	]
+}
---
+++ lab/week04/BoundingBox_Script.js
@@ -0,0 +1,113 @@
+function findTopLeft(pointList) {
+	var maxLat = null;
+	var minLon = null;
+	
+	pointList.data.forEach(
+		point => {
+			if (maxLat == null) {
+				maxLat = point.lat
+				maxLon = point.lon
+			}
+			else {
+				if (point.lat > maxLat){
+					maxLat = point.lat
+				}
+				if (point.lon < minLon){
+					minLon = point.lon
+				}
+			}
+		}
+		)
+	
+	return {"pointId": 1, "lat": maxLat, "lon": minLon}
+	}
+	
+function findTopRight(pointList){
+	var maxLat = null;
+	var maxLon = null;
+	
+	pointList.data.forEach(
+		point => {
+			if (maxLat == null) {
+				maxLat = point.lat
+				maxLon = point.lon
+			}
+			else {
+				if (point.lat > maxLat){
+					 maxLat = point.lat
+				}
+				if (point.lon > maxLon){
+					maxLon = point.lon
+				}
+			}
+		}
+		)
+		
+	return {"pointId": 2, "lat": maxLat, "lon": maxLon}
+}
+
+function findBottomLeft(pointList){
+	var minLat = null;
+	var minLon = null;
+	
+	pointList.data.forEach(
+		point => {
+			if (minLat == null) {
+				minLat = point.lat
+				minLon = point.lon
+			}
+			else {
+				if (point.lat < minLat){
+					 minLat = point.lat
+				}
+				if (point.lon < minLon){
+					minLon = point.lon
+				}
+			}
+		}
+		)
+		
+	return {"pointId": 3, "lat": minLat, "lon": minLon}
+}
+
+function findBottomRight(pointList){
+	var minLat = null;
+	var maxLon = null;
+	
+	pointList.data.forEach(
+		point => {
+			if (minLat == null) {
+				minLat = point.lat
+				maxLon = point.lon
+			}
+			else {
+				if (point.lat < minLat){
+					 minLat = point.lat
+				}
+				if (point.lon > maxLon){
+					 minLat = point.lon
+				}
+			}
+		}
+		)
+		
+	return {"pointId": 4, "lat": minLat, "lon": maxLon}
+}
+
+
+function Run(){
+	var topLeftPoint = findTopLeft(theJSON);
+	var topRightPoint = findTopRight(theJSON);
+	var bottomLeftPoint = findBottomLeft(theJSON);
+	var bottomRightPoint = findBottomRight(theJSON);
+	
+	var boundingBox = {
+		topLeft: topLeftPoint, 
+		topRight: topRightPoint, 
+		bottomLeft: bottomLeftPoint,
+		bottomRight: bottomRightPoint
+	}
+	
+	console.log("Bounding box computed for emilymcdonald@tamu.edu: ");
+	console.log(boundingBox);
+}
